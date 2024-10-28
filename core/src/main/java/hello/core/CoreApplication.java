ede74580db50e08cb6cf72a360826ef07dc1cce9
이승훈 사원 은행/카드DX2팀 (seunghoonlee@lgcns.com <seunghoonlee@cns.lgcns.com>
2024-10-28 15:21:02 +0900

    init settings

--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -1,4 +1,4 @@
-package jakarta;
+package hello.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -1,4 +1,4 @@
-package jakarta;
+package hello.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
