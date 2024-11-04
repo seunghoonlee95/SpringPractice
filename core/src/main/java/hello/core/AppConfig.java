f1064c1b558bbe34489678c299552de1e49def5a
이승훈 사원 은행/카드DX2팀 (seunghoonlee@lgcns.com <seunghoonlee@cns.lgcns.com>
2024-11-04 14:54:26 +0900

    의존관계 자동 주입 - 옵션 처리

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -11,26 +11,31 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
 
     @Bean
+    @Primary // 기본으로 사용할 빈으로 설정
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
+    @Primary // 기본으로 사용할 빈으로 설정
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
+    @Primary // 기본으로 사용할 빈으로 설정
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     @Bean
+    @Primary // 기본으로 사용할 빈으로 설정
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
