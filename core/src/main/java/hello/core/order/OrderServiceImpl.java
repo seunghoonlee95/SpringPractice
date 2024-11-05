1c118b747ffbfdb065c42285b1131ecf9911c2a6
이승훈 사원 은행/카드DX2팀 (seunghoonlee@lgcns.com <seunghoonlee@cns.lgcns.com>
2024-11-05 10:16:57 +0900

    @Autowired 필드명, @Qualifier, @Primary

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,15 +3,21 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+
+    //생성자가 하나밖에 없는 경우 Autowired annotation생략 가능
+    // 위에 lombok을 활용해 만든 @RequiredArgsConstructor가 아래의 constructor를 자동으로 내부적으로 생성해줌!
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
 
